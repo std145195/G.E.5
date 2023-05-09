74a7efebc2c255127fa2ec106e261d4d25502d8e
std145195 <std145195@ac.eap.gr>
2023-05-09 22:13:05 +0300

    lathos ektyposi

--- src/main/java/org/example/Demo.java
+++ src/main/java/org/example/Demo.java
@@ -8,7 +8,28 @@ public class Demo {
     static List<IGrade> studentsWhoFail = new ArrayList<>();
 
     public static void main(String[] args) {
-        // Προσθέστε κώδικα εδώ
+
+        Student std1 = new Student("131313","test","std@fom.com","eap");
+        Student std2 = new Student("121545","test2","hfdos@fom.com","eap");
+        Student std3 = new Student("164894","test3","dfa@fom.com","eap");
+        Student std4 = new Student("269894","test4","hjrj@fom.com","eap");
+        Student std5 = new Student("989654","test5","yejemy@fom.com","eap");
+
+        addToList(std1);
+        addToList(std2);
+        addToList(std3);
+        addToList(std4);
+        addToList(std5);
+
+        filterList(gradedStudents);
+
+        System.out.println("Φοιτητές που πέρασαν");
+        for (IGrade stp: studentsWhoPass)
+            System.out.println(stp.showAverage());
+
+        System.out.println("\nΦοιτητές που κόπηκαν");
+        for (IGrade stf: studentsWhoFail)
+            System.out.println(stf.showAverage());
     }
 
     static void addToList(IGrade graded) {
@@ -16,6 +37,11 @@ public class Demo {
     }
 
     static void filterList(List<IGrade> gradedStudents) {
-
+        for (IGrade st: gradedStudents){
+            if (st.showAverage()<5)
+                studentsWhoFail.add(st);
+            else
+                studentsWhoPass.add(st);
+        }
     }
 }
